4649b2a720a836d6290ec3da8ff67b02e128562b
EdinburghChen <111228850+EdinburghChen@users.noreply.github.com>
2023-09-11 08:20:27 +0800

    Update LuckyLottoStreamlit.py

--- LuckyLottoStreamlit.py
+++ LuckyLottoStreamlit.py
@@ -124,7 +124,7 @@ del cursor
 #conn.close() 
 # 加入網頁文字內容
 st.write("台灣彩券擷取資料中...")
-st.write("STEP #1 Complete!!")
+#st.write("STEP #1 Complete!!")
 
 try:
   # 建立資料庫連線 SQLite
@@ -175,7 +175,7 @@ try:
 
   #網頁顯示資料
   st.write("資料分析中")
-  st.write("STEP #2 Complete!!")
+  #st.write("STEP #2 Complete!!")
   st.write("A.近5期已開出獎號("+str(len(issued))+"/49):")
   st.write(str(issued))
   st.write("B.開出獎號>1次("+str(len(issuedbigone))+"/"+str(len(issued))+"):")
@@ -183,6 +183,19 @@ try:
   st.write("C.近5期未開出獎號("+str(len(unissued))+"/49):")
   st.write(str(unissued))
 
+  if st.button('A7', type="primary"):
+   #luckyNo
+   luckyNo=rd.sample(issued, k=7)  #近5期
+   #luckyNo2=rd.sample(numbers, k=1)
+   #luckyNo.extend(luckyNo2)
+   mA7=message+(str(luckyNo))
+   # HTTP 標頭參數與資料
+   headers = {"Authorization": "Bearer " + token}
+   data = {'message': mA7}
+   # 以 requests 發送 POST 請求
+   #requests.post("https://notify-api.line.me/api/notify",headers=headers, data=data)
+   st.write("A7:" + mA7)
+  
   if st.button('A6C1', type="primary"):
    #luckyNo
    luckyNo=rd.sample(issued, k=6)  #近5期
@@ -194,8 +207,20 @@ try:
    data = {'message': mA6C1}
    # 以 requests 發送 POST 請求
    requests.post("https://notify-api.line.me/api/notify",headers=headers, data=data)
-   st.write(mA6C1)
+   st.write("A6C1:" + mA6C1)
 
+  if st.button('A4C3', type="primary"):
+   #luckyNo
+   luckyNo=rd.sample(issued, k=4)  #近5期1次以上
+   luckyNo2=rd.sample(numbers, k=3)
+   luckyNo.extend(luckyNo2)
+   mA4C3=message+(str(luckyNo))
+   # HTTP 標頭參數與資料
+   headers = {"Authorization": "Bearer " + token}
+   data = {'message': mA4C3}
+   # 以 requests 發送 POST 請求
+   #requests.post("https://notify-api.line.me/api/notify",headers=headers, data=data)
+   st.write("A4C3:"+ mA4C3)
 
   if st.button('B4C3', type="primary"):
    #luckyNo
@@ -208,7 +233,7 @@ try:
    data = {'message': mB4C3}
    # 以 requests 發送 POST 請求
    requests.post("https://notify-api.line.me/api/notify",headers=headers, data=data)
-   st.write(mB4C3)
+   st.write("B4C3:" + mB4C3)
 
   if st.button('B3C4', type="primary"):
    #luckyNo
@@ -221,7 +246,7 @@ try:
    data = {'message': mB3C4}
    # 以 requests 發送 POST 請求
    #requests.post("https://notify-api.line.me/api/notify",headers=headers, data=data)
-   st.write(mB3C4)
+   st.write("B3C4:" + mB3C4)
     
 except Exception as e:
   st.write("Error: %s" % e)
