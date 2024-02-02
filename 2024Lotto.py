1fa3879ec2a996f4fbbbbd9b425963827c3c9281
EdinburghChen <111228850+EdinburghChen@users.noreply.github.com>
2024-02-02 15:20:33 +0800

    Update 2024Lotto.py

--- 2024Lotto.py
+++ 2024Lotto.py
@@ -221,8 +221,8 @@ except Exception as e:
 st.divider()
 #inputdate=st.date_input('獎號日期',datetime.date(2024, 1, 1))
 inputdate=st.date_input('獎號日期',datetime.date.today())
-data_Info_Dict = (st.text_input('請輸入號碼')).split(' ')
-#st.write('輸入號碼:', *data_Info_Dict)
+data_Info_Dict = (st.text_input('請輸入號碼')).split(',')
+st.write('輸入號碼:', *data_Info_Dict)
 if st.button('新增獎號', type="primary"):
    #st.write('新增獎號：'+ inputdate.strftime('%Y-%m-%d')+data_Info_Dict)  
   #新增--
