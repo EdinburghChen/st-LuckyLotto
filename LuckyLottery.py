90f6961472a30fff2eedb4bcb2dc7bd7dc0c4ff8
EdinburghChen <111228850+EdinburghChen@users.noreply.github.com>
2024-02-17 17:07:08 +0800

    Update LuckyLottery.py

--- LuckyLottery.py
+++ LuckyLottery.py
@@ -221,6 +221,7 @@ except Exception as e:
 
 
 st.divider()
+#inputdate=st.date_input('獎號日期',datetime.date(2024, 1, 1))
 inputdate=st.date_input('開獎日期',datetime.date.today())
 if st.button('同步開獎資料', type="primary"):
    st.write(inputdate)
